a24afcf57ef03010e60ae511df49a745503ec5da
psawla24 <pooja.sawla@gmail.com>
2014-10-12 01:47:58 -0700

    Added 3d photo cubes for venues and Rspec 75% complete

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,11 +1,16 @@
 class Survey < ActiveRecord::Base
 
+	#attr_accessor :people, :name, :location
+
   validates_inclusion_of :people, in: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   #validates :name, format: { with: /\A[a-zA-Z]+\z/ }
   #validates_format_of :name, :with => /^[a-zA-Z]$/ 
   #validates_format_of :name, :with => /^[a-zA-Z]$/ #, :on => :create
   #validates :name, length: { minimum: 10 }
-  validates_presence_of :name
+  validates :name, length: { maximum: 25 }
+  validates_presence_of :people, :name, :location, :on => :create
+  validates_format_of :location, :with => /^[A-Za-z0-9,. ]+$/, :on => :create, :multiline => true
+
 
 
 end
